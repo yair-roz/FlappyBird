274c032413a6100f20ab6cd864aacfb610bb0e04
yair-roz <yairrosoff@gmail.com>
2025-06-09 20:46:38 +0300

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new ScreenManager();
-            }
-        });
+        new ScreenManager();
     }
 }
